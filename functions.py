c9d579d9d3e2b01fa0130e714797750bdf112aca
Joao Iguana <jiboiadamor@gmail.com>
2024-03-19 09:31:31 +0000

    learned how to build functions in python

---
+++ functions.py
@@ -0,0 +1,13 @@
+def calculate_area(length, width):
+    area = length * width
+    print(area)
+
+calculate_area(18, 5)
+
+def is_even(number):
+    if number % 2 == 0:
+        print(True)
+    else:
+        print(False)
+
+is_even(18)
